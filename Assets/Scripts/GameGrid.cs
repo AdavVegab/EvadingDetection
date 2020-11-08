9d09bd46c375b107c6c9870e4ba1a82d028f0721
Andrés D. Vega B <andresvegabotero@gmail.com>
2020-11-08 18:10:37 +0100

    Adds the Grid

---
+++ Assets/Scripts/GameGrid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGrid : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] rows = new GameObject[10];
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GridRow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridRow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Cells")]
+    public GameObject [] cells = new GameObject[7];
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
